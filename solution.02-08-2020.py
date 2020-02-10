5f88655ff10af67e9fd07bf5d46ac922fe58f229
Alex R <alejandro.ramirez@jonajo.com>
2020-02-09 23:05:59 -0600

    serialize

--- solution.02-08-2020.py
+++ solution.02-08-2020.py
@@ -17,112 +17,40 @@ class Node:
         self.right = right
 
 
-SEPARATOR = '_'
+# Constants
+ROOT = Node('root', Node('left', Node('left.left')), Node('right'))
+SEPARATOR = '  '
 
 
-def traverse(node, index, array):
-    # Append first the parent child
-    parsed_val = node.val if node is not None else 'X'
-    array.append((index, parsed_val))
-    print(parsed_val)
+def serialize(node, show=False):
+    nodes = []
 
-    # Then traverse the left
-    if node is not None:
-        traverse(node.left, index * 2, array)
-        traverse(node.right, (index * 2) + 1, array)
+    def helper(node, i):
+        if node:
+            nodes.append((i, node.val))
+        elif node is None:
+            nodes.append((i, 'X'))
 
+        if node is not None:
+            helper(node.left, i * 2)
+            helper(node.right, i * 2 + 1)
 
-def sort_tuples(tuples):
-    # Sort the tuples
-    tuples.sort(key=lambda x: x[0])
+    # Generate
+    helper(node, 1)
 
-    # "Flatten" the tuples in the array
-    result = []
-    for tuple_item in tuples:
-        result.append(tuple_item[1])
+    # Sort
+    nodes.sort(key=lambda x: x[0])
 
-    # Return array representation of BST
-    return result
+    # Remove index
+    final = map(lambda x: x[1], nodes)
 
+    # Generate string
+    s = SEPARATOR.join(list(final))
 
-def serialize(root):
-    # Traverse Nodes and generate a BST array (unsorted)
-    array = []
-    traverse(root, 1, array)
+    if show:
+        print(s)
 
-    # Sort the array
-    array_bst = sort_tuples(array)
-    print({'array_bst': array_bst}, '\n')
-
-    return SEPARATOR.join(array_bst)
+    return
 
 
-def deserialize(s):
-    print(s)
-
-    # Split from string using separator
-    separated = s.split(SEPARATOR)
-
-    # Remove last item since, invariantly, will always be empty string
-    del separated[len(separated) - 1]
-
-    # print(separated)
-
-    # Loop through items and rebuild BST nodes
-    def build(index, array):
-        i = index - 1
-
-        # e.g.: [1]
-        if i == (len(array) - 1):
-            return Node(array[i - 1], None, None)
-
-        else:
-            left = build(i * 2, array)
-            right = build((i * 2) + 1, array)
-            return Node(array[i - 1], left, right)
-
-    return build(1, [2])
-
-
-def rebuild(root, i, array):
-    if i >= len(array):
-        return
-
-    left = rebuild(array[i * 2], i, array)
-    right = rebuild(array[(i * 2) + 1], i, array)
-    return Node(array[i], left, right)
-
-
-# node = Node(
-#     'root',
-#     Node(
-#         'left',
-#         Node('left.left')
-#     ),
-#     Node('right')
-# )
-
-# node = Node('root', Node('left', Node('left.left')), Node('right'))
-# serialized_node = serialize(node)
-# serial_deserial = deserialize(serialized_node)
-
-
-def main():
-    def build(index, array):
-        i = index - 1
-
-        # e.g.: [1]
-        if i == (len(array) - 1):
-            return Node(array[i - 1], None, None)
-
-        else:
-            left = build(i * 2, array)
-            right = build((i * 2) + 1, array)
-            return Node(array[i - 1], left, right)
-
-    return build(1, [2])
-
-
-r = main()
-traverse(r[0], 0, r)
-print(main())
+serialize(ROOT, True)
