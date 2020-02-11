8760a075904f5bca37e77f3947500c02519994e1
Alex R <alejandro.ramirez@jonajo.com>
2020-02-11 12:05:37 -0600

    Solved (finally!). Added Lisp solution

--- solution.02-08-2020.py
+++ solution.02-08-2020.py
@@ -23,7 +23,8 @@ SEPARATOR = '  '
 EMPTY = 'X'
 
 
-def serialize(node, show=False):
+# O(2n)
+def serialize(node):
     nodes = []
 
     def helper(node):
@@ -37,15 +38,29 @@ def serialize(node, show=False):
             helper(node.left)
             helper(node.right)
 
+    # O(n)
     helper(node)
+
+    # O(n) for joining string
     return SEPARATOR.join(nodes)
 
 
+# O(n)
+def lisp_serialize(node):
+    if node is None:
+        return EMPTY
+
+    right = lisp_serialize(node.right)
+    left = lisp_serialize(node.left)
+    return '({} ({} {}))'.format(node.val, left, right)
+
+
+# O(n)
 def deserialize(s):
     # Returns a node with all the left/right
     def helper():
         val = next(vals)
-        print('NEXT VALS ==>', val)
+        # print('NEXT VALS ==>', val)
 
         # If we reach the end, then it's a leaf and it's the last one
         if val is None or val is 'X':
@@ -64,9 +79,19 @@ def deserialize(s):
 
 
 # Serialize
-l = serialize(ROOT)
-print(l)
+s = serialize(ROOT)
+print(s)
+
+# Serialize similar to lisp
+lisp = lisp_serialize(ROOT)
+print(lisp)
 
 # Deserialize
-d = deserialize(l)
+d = deserialize(s)
 print(serialize(d))
+
+try:
+    assert deserialize(serialize(ROOT)).left.left.val == 'left.left'
+    print("Test passed!")
+except AssertionError as error:
+    print("Test failed :(")
