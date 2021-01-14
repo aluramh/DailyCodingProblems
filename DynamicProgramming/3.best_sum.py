362d7e79c335fa5b1fa5c8f32a915b8b295e591e
Alex R <alejandro.herrera@ciitizen.com>
2021-01-14 11:10:31 -0600

    Added best sum

--- DynamicProgramming/3.best_sum.py
+++ DynamicProgramming/3.best_sum.py
@@ -2,6 +2,7 @@ def best_sum_recursive(target_sum, numbers):
     if target_sum == 0: return []
     if target_sum < 0: return None
 
+    # Is independant for each loop. Is not shared between recursions.
     shortest_combination = None
 
     for num in numbers:
@@ -18,16 +19,17 @@ def best_sum_recursive(target_sum, numbers):
     return shortest_combination
 
 
-def best_sum(target_sum, numbers, memo={}):
+def best_sum(target_sum, numbers, memo):
     if target_sum in dict.keys(memo): return memo[target_sum]
     if target_sum == 0: return []
     if target_sum < 0: return None
 
+    # Each recursion as its own shortest_combination
     shortest_combination = None
 
     for num in numbers:
         remainder = target_sum - num
-        remainder_combination = best_sum(remainder, numbers)
+        remainder_combination = best_sum(remainder, numbers, memo)
 
         if remainder_combination is not None:
             combination = remainder_combination + [num]
@@ -36,41 +38,32 @@ def best_sum(target_sum, numbers, memo={}):
                                                   len(shortest_combination)):
                 shortest_combination = combination
 
+    memo[target_sum] = shortest_combination
     return shortest_combination
 
 
 if __name__ == "__main__":
     try:
-        # ANCHOR: - Recursion
+        print(best_sum(7, [5, 3, 4, 7], {}))
+        assert best_sum(7, [5, 3, 4, 7], {}) == [7]
 
-        result = best_sum_recursive(7, [5, 3, 4, 7])
+        print(best_sum(8, [2, 3, 5], {}))
+        assert best_sum(8, [2, 3, 5], {}) == [5, 3]
+
+        print(best_sum(7, [5, 3, 6], {}))
+        assert best_sum(7, [5, 3, 6], {}) is None
+
+        result = best_sum(8, [2, 3, 5], {})
         print(result)
-        assert isinstance(result, list) is True
+        assert result == [5, 3]
 
-        result = best_sum_recursive(7, [5, 3, 6])
+        result = best_sum(100, [1, 2, 5, 25], {})
         print(result)
-        assert isinstance(result, list) is False
+        assert result == [25, 25, 25, 25]
 
-        # Deep recursion problem => Stack overflow
-        # result = how_sum_recursive(300, [7, 14])
-        # assert (isinstance(result, list) is None)
-
-        # ANCHOR: - Dynamic programming
-
-        # result = best_sum(7, [5, 3, 4, 7])
-        # print(result)
-        # assert isinstance(result, list) is True
-
-        # result = best_sum(7, [5, 3, 6])
-        # print(result)
-        # assert isinstance(result, list) is False
-
-        # result = best_sum(8, [2, 3, 5])
-        # print(result)
-
-        # result = best_sum(300, [7, 14])
-        # print(result)
-        # assert isinstance(result, list) is False
+        result = best_sum(300, [7, 14], {})
+        print(result)
+        assert result is None
 
         print("Program finished with no errors 😉")
 
