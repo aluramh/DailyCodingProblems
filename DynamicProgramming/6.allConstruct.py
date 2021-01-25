87358bb26893d184a93e072ead6f78bdb57ae2dc
Alex R <alejandro.herrera@ciitizen.com>
2021-01-25 13:10:23 -0600

    works

--- DynamicProgramming/6.allConstruct.py
+++ DynamicProgramming/6.allConstruct.py
@@ -1,4 +1,4 @@
-def count_construct_RECURSIVE(target, wordbank):
+def all_construct_RECURSIVE(target, wordbank):
     if target == '':
         return 1
 
@@ -11,60 +11,66 @@ def count_construct_RECURSIVE(target, wordbank):
         if target.find(word) == 0:
             # Remove the prefix from the string to create a new target
             new_target = target[target.index(word) + len(word):len(target)]
-            num_ways_rest = count_construct_RECURSIVE(new_target, wordbank)
+            num_ways_rest = all_construct_RECURSIVE(new_target, wordbank)
 
             total_count += num_ways_rest
 
     return total_count
 
 
-def count_construct(target, wordbank, memo):
+def all_construct(target, wordbank, memo):
     if target in dict.keys(memo):
         return memo[target]
 
     if target == '':
-        return 1
+        return [[]]
 
     # This is separate for each stackcall
-    total_count = 0
+    result = []
 
     # We want to loop through EVERY word ALWAYS to see how many can build the target
     for word in wordbank:
         # Check if the current word is a prefix of the target
         if target.find(word) == 0:
             # Remove the prefix from the string to create a new target
-            new_target = target[target.index(word) + len(word):len(target)]
-            num_ways_rest = count_construct(new_target, wordbank, memo)
+            suffix = target[target.index(word) + len(word):len(target)]
 
-            total_count += num_ways_rest
+            # Returns an array of all the ways to build the suffix
+            suffix_ways = all_construct(suffix, wordbank, memo)
 
-    memo[target] = total_count
+            # Get the target ways. For each way, append the current word iteration
+            target_ways = list(map(lambda way: [word] + way, suffix_ways))
+
+            # Return the result
+            result += target_ways
+
+    memo[target] = result
     return memo[target]
 
 
 if __name__ == "__main__":
     try:
-        # print(can_construct("skateboard", ['skate', "board"], {}))
-        # print(
-        #     can_construct("enterapotentpot",
-        #                   ['a', "p", "ent", "enter", "ot", "o", "t"], {}))
+        print(all_construct(
+            "skateboard",
+            ['skate', "board"],
+            {},
+        ))
 
         print(
-            count_construct(
+            all_construct(
                 "enterapotentpot",
                 ['a', "p", "ent", "enter", "ot", "o", "t"],
                 {},
             ))
 
-        print(
-            count_construct(
-                "purple",
-                ['purp', "p", "ur", "le", "purpl"],
-                {},
-            ))
+        print(all_construct(
+            "purple",
+            ['purp', "p", "ur", "le", "purpl"],
+            {},
+        ))
 
         print(
-            count_construct("eeeeeeeeeeeeeeeeeeeeeeeef", [
+            all_construct("eeeeeeeeeeeee", [
                 'e',
                 "ee",
                 "eee",
