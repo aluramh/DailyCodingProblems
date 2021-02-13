a96b61b78b00f5dd087fa6272c2c8838eb3b8b80
Alex R <alejandro.herrera@ciitizen.com>
2021-02-13 16:10:57 -0600

    hgsum

--- HackerRank/HourglassSum.py
+++ HackerRank/HourglassSum.py
@@ -1,67 +1,44 @@
-arr_subset = [
-    [1, 1, 1, 0, 0, 0],
-    [0, 1, 0, 0, 0, 0],
-    [1, 1, 1, 0, 0, 0],
-    [0, 0, 2, 4, 4, 0],
-    [0, 0, 0, 2, 0, 0],
-    [0, 0, 1, 2, 4, 0],
-]
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
 
 
+# Complete the hourglassSum function below.
 def hourglassSum(arr):
-    def convert_to_matrix(arr):
-        lines = arr.split('\n')
-        matrix = list(
-            map(lambda line: list(map(lambda x: int(x), line.split())), lines))
-        return list(matrix)
-
-    m = convert_to_matrix(arr)
-
-    # We'll use i and j to traverse and it will limit our values so we don't
-    # grab an out of bounds item
-
-    memo = {}
+    """
+    We'll use i and j to traverse and it will limit our values so we don't
+    grab an out of bounds item
+    """
     max_sum = None
     j = 0
     for i in range(4):
         for j in range(4):
-            hourglass_sum = m[i][j] + m[i][j + 1] + m[i][j + 2]
-            hourglass_sum += m[i + 1][j + 1]
-            hourglass_sum += m[i + 2][j] + m[i + 2][j + 1] + m[i + 2][j + 2]
+            hourglass_sum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2]
+            hourglass_sum += arr[i + 1][j + 1]
+            hourglass_sum += arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j +
+                                                                            2]
 
             if max_sum is None or max_sum < hourglass_sum:
                 max_sum = hourglass_sum
 
-            key = f"{i}{j}"
-            memo[key] = hourglass_sum
-            if key == "12":
-                print()
-
+    print(max_sum)
     return max_sum
+ 
 
+if __name__ == '__main__':
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-tests = [
-    ("""1 1 1 0 0 0
-        0 1 0 0 0 0
-        1 1 1 0 0 0
-        0 0 2 4 4 0
-        0 0 0 2 0 0
-        0 0 1 2 4 0""", 19),
-    ("""0 0 0 0 0 0
-        0 0 0 0 0 0
-        0 0 0 0 0 0
-        0 0 0 0 0 0
-        0 0 0 0 0 0
-        0 0 0 0 0 0""", 0),
-    ("""0 0 0 0 0 0
-        0 0 0 1 0 0
-        0 0 0 0 0 0
-        1 0 0 0 0 0
-        0 -2 0 0 0 0
-        0 0 0 0 0 0""", 1),
-]
+    arr = []
 
-for arr, expected_result in tests:
-    r = hourglassSum(arr)
-    print(r)
-    assert r == expected_result
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = hourglassSum(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
